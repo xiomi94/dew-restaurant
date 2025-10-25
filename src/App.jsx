5158c6638a153ac6d943f271bc3bceb7dbdc0bf9
xiomi94 <xiomarajimvel94@gmail.com>
2025-10-25 19:13:02 +0100

    Create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
     <>
+      <Header/>
     </>
   )
 }
 
-export default App
+export default App;
